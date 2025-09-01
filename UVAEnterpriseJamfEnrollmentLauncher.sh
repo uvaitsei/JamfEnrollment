e97fd47ac7fb01408fc6898e87448b3268d19fc2
Matt McChesney <mam5hs@virginia.edu>
2025-09-01 12:56:18 -0400

    Update

--- UVAEnterpriseJamfEnrollmentLauncher.sh
+++ UVAEnterpriseJamfEnrollmentLauncher.sh
@@ -4,6 +4,13 @@ echo "Starting UVA Enterprise Jamf Enrollment Launcher..."
 # URL of the script to download
 SCRIPT_Name="EnrollmentLauncher.sh"
 SCRIPT_URL="https://raw.githubusercontent.com/uvaitsei/JamfEnrollment/refs/heads/main/UVAEnterpriseJamfEnrollment.sh"
+ASMAPI="/private/var/tmp/UVAASM/uva.asmprod.plist"
+
+# Confirm ASMAPI exists
+if [ ! -f "$ASMAPI" ]; then
+    echo "Error: $ASMAPI not found."
+    exit 1
+fi
 
 # Temporary file to store the downloaded script
 
