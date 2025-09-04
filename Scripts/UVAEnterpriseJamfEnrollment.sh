d6c0448f885645ed49882571dbaa6c4c940e3a58
Matt McChesney <mam5hs@virginia.edu>
2025-09-04 11:38:15 -0400

    Update UVAEnterpriseJamfEnrollment.sh

--- Scripts/UVAEnterpriseJamfEnrollment.sh
+++ Scripts/UVAEnterpriseJamfEnrollment.sh
@@ -77,10 +77,10 @@ ScriptLog="$ScriptLogPath$ScriptLogFile"
 TimeStamp=$(date +%Y-%m-%d\ %H:%M:%S)
 
 function CreateLogFile() {
-    if [[ ! -f "$ScriptLog" ]]; then
+	if [[ ! -f "$ScriptLog" ]]; then
 		mkdir -p "$ScriptLogPath"
-        touch "$ScriptLog"	
-    fi
+		touch "$ScriptLog"
+	fi
 }
 
 function UpdateScriptLog() {
